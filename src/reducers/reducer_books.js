28dfc68cf130618826629928d4d08df683ca94ec
Adam Kotas <akotas@future-processing.com>
2018-03-20 17:00:21 +0100

    action 2

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
     return [
-        { title: 'Ski jumping' },
-        { title: 'Football' },
-        { title: 'Running' },
-        { title: 'Ping-pong' },
-        { title: 'Snowboarding' }
+        { title: 'Ski jumping', pages: 101 },
+        { title: 'Football', pages: 10  },
+        { title: 'Running', pages: 1111  },
+        { title: 'Ping-pong', pages: 789  },
+        { title: 'Snowboarding', pages: 4321  }
     ]
 }
